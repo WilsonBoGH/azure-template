48c746650b7ef26e868713a75848ea525256f448
Wilson Bo <wilsonbo@lean-soft.cn>
2017-04-11 13:49:15 +0800

    create oper install shell

---
+++ InstallDockerOnOper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+AZUREUSER=${1}
+OPERDNS=${2}
+
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo groupadd docker
+sudo usermod -aG docker $AZUREUSER
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "{ \"insecure-registries\":[\"harbor.devopshub.cn\",\"$OPERDNS:5000\"] }" | sudo tee /etc/docker/daemon.json
+sudo service docker restart
+
+printf "\nCiphers aes128-cbc,aes192-cbc,aes256-cbc,aes128-ctr,aes192-ctr,aes256-ctr,3des-cbc,arcfour128,arcfour256,arcfour,blowfish-cbc,cast128-cbc" | sudo tee -a /etc/ssh/sshd_config
+printf "\nMACs hmac-md5,hmac-sha1,umac-64@openssh.com,hmac-ripemd160,hmac-sha1-96,hmac-md5-96" | sudo tee -a /etc/ssh/sshd_config
+printf "\nKexAlgorithms diffie-hellman-group1-sha1,diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha1,diffie-hellman-group-exchange-sha256,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group1-sha1,curve25519-sha256@libssh.org" | sudo tee -a /etc/ssh/sshd_config
+
+service ssh restart
+
+sudo apt-get install openjdk-8-jdk
